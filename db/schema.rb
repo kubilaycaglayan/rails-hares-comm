a0df5a6e09e1a1d904db9cb52ef978196b23599d
kubilaycaglayan <kblycaglayan@gmail.com>
2024-10-10 15:31:33 +0300

    Create order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_09_153129) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_10_123042) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,4 +22,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_09_153129) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "orders", force: :cascade do |t|
+    t.bigint "customer_id", null: false
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id"], name: "index_orders_on_customer_id"
+  end
+
+  add_foreign_key "orders", "customers"
 end
